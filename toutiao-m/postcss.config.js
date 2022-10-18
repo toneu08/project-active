37a8a07701002a91c9ec60d79234a91f336a7f74
tonglu <lu_toneu@163.com>
2022-10-18 19:03:08 +0800

    项目初始化完成1.0

---
+++ toutiao-m/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    'postcss-pxtorem': {
+      // rootValue: 37.5,
+      rootValue ({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75
+      },
+      propList: ['*'],
+    },
+  },
+};
--- toutiao-m/src/main.js
+++ toutiao-m/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant' // 加载 Vant 核心组件库
+import 'vant/lib/index.css' // 加载 Vant 全局样式
+import 'amfe-flexible' // 加载动态设置Rem基准值
+
 import './styles/index.less' // 加载全局样式
+
+Vue.use(Vant) // 注册使用 Vant 组件库
 
 Vue.config.productionTip = false
 
